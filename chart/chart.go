4363afa74a9b8c0b361a545cda2e654c64aa76c6
Kevin Schoon <kevinschoon@gmail.com>
2016-09-18 14:36:52 -0400

    Refactor Chart

--- chart/chart.go
+++ chart/chart.go
@@ -3,26 +3,34 @@ package chart
 import (
 	"github.com/gonum/plot"
 	"github.com/gonum/plot/plotter"
+	"github.com/gonum/plot/plotutil"
 	"github.com/gonum/plot/vg"
 	"github.com/gonum/plot/vg/draw"
 	"github.com/kevinschoon/gofit/models"
 	"image/color"
 )
 
-type PlotCfg struct {
+type Config struct {
 	PrimaryColor   color.Color
 	SecondaryColor color.Color
 	XLabel         string
 	YLabel         string
 	Title          string
+	Keys           []string
+	XAxis          models.Key
+	YAxis          map[string]models.Key
+	Width          vg.Length
+	Height         vg.Length
 }
 
-func getPlt(cfg PlotCfg) (*plot.Plot, error) {
+func getPlot(cfg Config) (*plot.Plot, error) {
 	plt, err := plot.New()
 	if err != nil {
 		return nil, err
 	}
 	plt.Legend.Color = cfg.PrimaryColor
+	plt.Legend.Top = true
+	plt.Legend.YOffs = 0.1 * vg.Inch
 	plt.BackgroundColor = cfg.SecondaryColor
 
 	plt.Title.Color = cfg.PrimaryColor
@@ -44,36 +52,38 @@ func getPlt(cfg PlotCfg) (*plot.Plot, error) {
 	plt.X.Tick.Color = cfg.PrimaryColor
 	plt.X.Tick.Label.Color = cfg.PrimaryColor
 	plt.X.Tick.Label.Font.Size = 0.2 * vg.Inch
-	//plt.X.Tick.Marker = plot.UnixTimeTicks{Format: "2006-01-02"}
+	plt.X.Tick.Marker = plot.UnixTimeTicks{Format: "2006-01-02"}
 	return plt, nil
 }
 
-func getXYs(collection *models.Collection, x, y models.Key) plotter.XYer {
-	xys := make(plotter.XYs, collection.Len())
-	for i, series := range collection.Series {
-		xys[i].X = series.Sum(x)
-		xys[i].Y = series.Sum(y)
-	}
-	return xys
-}
-
-// DistanceOverTime returns a chart displaying daily distance totals
-func New(collection *models.Collection, x, y models.Key) (vg.CanvasWriterTo, error) {
-	plt, _ := getPlt(PlotCfg{
-		PrimaryColor:   color.White,
-		SecondaryColor: color.Black,
-		Title:          collection.Name,
-		YLabel:         collection.GetName(y),
-		XLabel:         collection.GetName(x),
-	})
-	line, _ := plotter.NewLine(getXYs(collection, x, y))
-	line.LineStyle.Color = color.White
-	line.LineStyle.Width = vg.Points(2)
-	plt.Add(plotter.NewGrid())
-	plt.Add(line)
-	canvas, err := draw.NewFormattedCanvas(32*vg.Inch, 12*vg.Inch, "png")
+// New returns a line chart plotting the provided values
+func New(cfg Config, series []*models.Series) (vg.CanvasWriterTo, error) {
+	plt, err := getPlot(cfg)
 	if err != nil {
-		panic(err)
+		return nil, err
+	}
+	// Implement variadic arguments for plotter.AddLines
+	// ...string,XYer
+	data := make([]interface{}, 0)
+	xValues := models.Select(cfg.XAxis, series)
+	for _, name := range models.Keys(series[0]) {
+		if y, ok := cfg.YAxis[name]; ok {
+			xys := make(plotter.XYs, len(xValues))
+			for i, value := range models.Select(y, series) {
+				xys[i].X = xValues[i].Float64()
+				xys[i].Y = value.Float64()
+			}
+			data = append(data, name)
+			data = append(data, xys)
+		}
+	}
+	if err := plotutil.AddLines(plt, data...); err != nil {
+		return nil, err
+	}
+	plt.Add(plotter.NewGrid())
+	canvas, err := draw.NewFormattedCanvas(cfg.Width, cfg.Height, "png")
+	if err != nil {
+		return nil, err
 	}
 	plt.Draw(draw.New(canvas))
 	return canvas, nil
